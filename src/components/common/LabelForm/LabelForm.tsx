5ce82550753810833aee25eccde839a8b5dd4218
VictoryJu <kob0805@naver.com>
2024-07-29 23:49:51 +0900

    [refact] LabelForm Component refact

--- src/components/common/LabelForm/LabelForm.tsx
+++ src/components/common/LabelForm/LabelForm.tsx
@@ -1,6 +1,7 @@
-import React, { Children, ReactNode } from 'react';
+import React, { Children, ReactElement, ReactNode } from 'react';
 import styles from './LabelForm.module.scss';
 import CheckIcon from '@/icons/icon/CheckIcon';
+import ContentField from '../ContentField/ContentField';
 
 interface LabelFormProps {
   children: ReactNode;
@@ -10,7 +11,7 @@ const LabelForm = ({ children, className }: LabelFormProps) => {
   return <div className={className}>{children}</div>;
 };
 
-interface HeaderTitleProps {
+interface TitleProps {
   children?: ReactNode;
   title: string;
   className?: string;
@@ -25,51 +26,86 @@ const getChildren = (children: ReactNode, type: unknown) => {
   return filterChildArray.length > 0 ? filterChildArray : false;
 };
 
-const HeaderTitle = ({ children, title, className }: HeaderTitleProps) => {
-  const subLabel = getChildren(children, HeaderSubLabelType);
-  const label = getChildren(children, HeaderLabelType);
+const Title = ({ children, title, className }: TitleProps) => {
+  const subLabel = getChildren(children, SubLabelType);
+  const label = getChildren(children, LabelType);
   return (
-    <div className={styles.HeaderTitleWrapper}>
-      <div className={styles.HeaderTitleContainer}>
-        <div className={`${styles.HeaderTitle} ${className}`}>{title}</div>
+    <div className={styles.TitleWrapper}>
+      <div className={styles.TitleContainer}>
+        <div className={`${styles.Title} ${className}`}>{title}</div>
         {label && <>{label}</>}
       </div>
-      {subLabel && <div className={styles.HeaderSubLabel}>{subLabel}</div>}
+      {subLabel && <div className={styles.SubLabel}>{subLabel}</div>}
     </div>
   );
 };
 
-interface HeaderSubLabelProps {
+interface SubLabelProps {
   children?: ReactNode;
   className?: string;
 }
 
-const HeaderSubLabel = ({ children, className }: HeaderSubLabelProps) => {
+const SubLabel = ({ children, className }: SubLabelProps) => {
   return <div className={className}>{children}</div>;
 };
 
-interface HeaderLabelProps {
+interface LabelProps {
   require?: boolean;
   children?: ReactNode;
   className?: string;
 }
 
-const HeaderLabel = ({ require, children, className }: HeaderLabelProps) => {
+const Label = ({ require, children, className }: LabelProps) => {
   return require ? (
-    <div className={styles.HeaderLabelRequire}>
+    <div className={styles.LabelRequire}>
       <CheckIcon width={14} height={14} className={styles.CheckIcon} />
-      <span className={styles.HeaderLabelRequireText}>필수입력</span>
+      <span className={styles.LabelRequireText}>필수입력</span>
     </div>
   ) : (
     <div className={className}>{children}</div>
   );
 };
 
-const HeaderSubLabelType = (<HeaderSubLabel />).type;
-const HeaderLabelType = (<HeaderLabel />).type;
+interface ContentProps {
+  children: ReactElement<HTMLInputElement>;
+  backgroundColor?: 'Gray' | 'White';
+  border?: 'Border' | 'None';
+  className?: string;
+  onClick?: () => void;
+}
+const Content = ({
+  children,
+  backgroundColor = 'White',
+  border = 'Border',
+  onClick,
+  className,
+}: ContentProps) => {
+  return (
+    <ContentField
+      onClick={onClick}
+      className={className}
+      backgroundColor={backgroundColor}
+      border={border}
+    >
+      {children}
+    </ContentField>
+  );
+};
 
-LabelForm.HeaderTitle = HeaderTitle;
-LabelForm.HeaderLabel = HeaderLabel;
-LabelForm.HeaderSubLabel = HeaderSubLabel;
+interface DescriptionProps {
+  children: ReactNode;
+}
+const Description = ({ children }: DescriptionProps) => {
+  return <div className={styles.Description}>{children}</div>;
+};
+
+const SubLabelType = (<SubLabel />).type;
+const LabelType = (<Label />).type;
+
+LabelForm.Title = Title;
+LabelForm.Label = Label;
+LabelForm.SubLabel = SubLabel;
+LabelForm.Content = Content;
+LabelForm.Description = Description;
 
 export default LabelForm;
--- src/pages/Onboarding/SignUp/SignupForm.tsx
+++ src/pages/Onboarding/SignUp/SignupForm.tsx
@@ -49,136 +49,109 @@ const SignupForm = () => {
 
   return (
     <>
-      <div className={styles.SignupFormWrapper}>
-        <LabelForm>
-          <LabelForm.HeaderTitle title="이메일">
-            <LabelForm.HeaderLabel require />
-          </LabelForm.HeaderTitle>
-        </LabelForm>
-        <ContentField
-          className={styles.SignupFormInputWrapper}
-          backgroundColor="White"
-          border="Border"
-        >
+      <LabelForm className={styles.SignupFormWrapper}>
+        <LabelForm.Title title="이메일">
+          <LabelForm.Label require />
+        </LabelForm.Title>
+        <LabelForm.Content className={styles.SignupFormInputWrapper}>
           <input
             className={styles.SignupFormInput}
             type="email"
             placeholder="mongle@mongle.com"
+            name="email"
           />
-        </ContentField>
-      </div>
-      <div className={styles.SignupFormWrapper}>
-        <LabelForm>
-          <LabelForm.HeaderTitle title="비밀번호">
-            <LabelForm.HeaderLabel require />
-          </LabelForm.HeaderTitle>
-        </LabelForm>
-        <ContentField
-          className={styles.SignupFormInputWrapper}
-          backgroundColor="White"
-          border="Border"
-        >
+        </LabelForm.Content>
+      </LabelForm>
+
+      <LabelForm className={styles.SignupFormWrapper}>
+        <LabelForm.Title title="비밀번호">
+          <LabelForm.Label require />
+        </LabelForm.Title>
+        <LabelForm.Content className={styles.SignupFormInputWrapper}>
           <input
             className={styles.SignupFormInput}
-            type="email"
+            type="password"
             placeholder="비밀번호 조건"
+            name="password"
           />
-        </ContentField>
-        <div className={styles.SignupFormDescriptionWrapper}>
-          <div className={styles.SignupFormDescriptionLine}>
-            <CheckIcon width={14} height={14} className={styles.CheckIcon} />
-            <span>영문, 숫자, 특수문자 중 2가지 이상 조합</span>
+        </LabelForm.Content>
+        <LabelForm.Description>
+          <div className={styles.SignupFormDescriptionWrapper}>
+            <div className={styles.SignupFormDescriptionLine}>
+              <CheckIcon width={14} height={14} className={styles.CheckIcon} />
+              <span>영문, 숫자, 특수문자 중 2가지 이상 조합</span>
+            </div>
+            <div className={styles.SignupFormDescriptionLine}>
+              <CheckIcon width={14} height={14} className={styles.CheckIcon} />
+              <span>10자리 이상</span>
+            </div>
           </div>
-          <div className={styles.SignupFormDescriptionLine}>
-            <CheckIcon width={14} height={14} className={styles.CheckIcon} />
-            <span>10자리 이상</span>
-          </div>
-        </div>
-      </div>
-      <div className={styles.SignupFormWrapper}>
-        <LabelForm>
-          <LabelForm.HeaderTitle title="업체명">
-            <LabelForm.HeaderLabel require />
-          </LabelForm.HeaderTitle>
-        </LabelForm>
-        <ContentField
-          className={styles.SignupFormInputWrapper}
-          backgroundColor="White"
-          border="Border"
-        >
+        </LabelForm.Description>
+      </LabelForm>
+
+      <LabelForm className={styles.SignupFormWrapper}>
+        <LabelForm.Title title="업체명">
+          <LabelForm.Label require />
+        </LabelForm.Title>
+        <LabelForm.Content className={styles.SignupFormInputWrapper}>
           <input
             className={styles.SignupFormInput}
-            type="email"
             placeholder="몽글몽글 2호점"
           />
-        </ContentField>
-        <div className={styles.SignupFormDescriptionWrapper}>
+        </LabelForm.Content>
+        <LabelForm.Description>
           (직영 또는 가맹점이라면) 몇호점까지 입력해주세요.
-        </div>
-      </div>
-      <div className={styles.SignupFormWrapper}>
-        <LabelForm>
-          <LabelForm.HeaderTitle title="업체 연락처">
-            <LabelForm.HeaderLabel require />
-          </LabelForm.HeaderTitle>
-        </LabelForm>
-        <ContentField
-          className={styles.SignupFormInputWrapper}
-          backgroundColor="White"
-          border="Border"
-        >
+        </LabelForm.Description>
+      </LabelForm>
+
+      <LabelForm className={styles.SignupFormWrapper}>
+        <LabelForm.Title title="업체 연락처">
+          <LabelForm.Label require />
+        </LabelForm.Title>
+        <LabelForm.Content className={styles.SignupFormInputWrapper}>
           <input
             className={styles.SignupFormInput}
-            type="email"
+            type="tell"
             placeholder="010-0000-0000"
           />
-        </ContentField>
-        <div className={styles.SignupFormDescriptionWrapper}>
-          <CheckBox
-            width={20}
-            height={20}
-            isSelected={signupInfo.defaultPhone}
-          />
-          <span>기본 연락처와 동일</span>
-        </div>
-      </div>
+        </LabelForm.Content>
+        <LabelForm.Description>
+          <div className={styles.SignupFormDescriptionWrapper}>
+            <CheckBox
+              width={20}
+              height={20}
+              isSelected={signupInfo.defaultPhone}
+            />
+            <span>기본 연락처와 동일</span>
+          </div>
+        </LabelForm.Description>
+      </LabelForm>
+
       <div className={styles.SignupFormWrapper}>
         <LabelForm>
-          <LabelForm.HeaderTitle title="활동지역">
-            <LabelForm.HeaderLabel require />
-          </LabelForm.HeaderTitle>
+          <LabelForm.Title title="활동지역">
+            <LabelForm.Label require />
+          </LabelForm.Title>
         </LabelForm>
         <ContentField
           className={styles.SignupFormInputWrapper}
           backgroundColor="White"
           border="Border"
         >
-          <input
-            className={styles.SignupFormInput}
-            type="email"
-            placeholder="지역 선택"
-          />
+          <input className={styles.SignupFormInput} placeholder="지역 선택" />
         </ContentField>
       </div>
+
+      <LabelForm className={styles.SignupFormWrapper}>
+        <LabelForm.Title title="SNS 주소" />
+        <LabelForm.Content className={styles.SignupFormInputWrapper}>
+          <input className={styles.SignupFormInput} placeholder="업체명 입력" />
+        </LabelForm.Content>
+      </LabelForm>
+
       <div className={styles.SignupFormWrapper}>
         <LabelForm>
-          <LabelForm.HeaderTitle title="SNS 주소" />
-        </LabelForm>
-        <ContentField
-          className={styles.SignupFormInputWrapper}
-          backgroundColor="White"
-          border="Border"
-        >
-          <input
-            className={styles.SignupFormInput}
-            type="email"
-            placeholder="업체명 입력"
-          />
-        </ContentField>
-      </div>
-      <div className={styles.SignupFormWrapper}>
-        <LabelForm>
-          <LabelForm.HeaderTitle title="현재 이용 중인 예약 플랫폼" />
+          <LabelForm.Title title="현재 이용 중인 예약 플랫폼" />
         </LabelForm>
         <div className={styles.SignupFormPlatformWrapper}>
           {platformList.map((platfrom, idx) => (
