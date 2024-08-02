b272517581fb22163b622dcafd2331cf71e631c9
VictoryJu <kob0805@naver.com>
2024-08-03 00:59:27 +0900

    [refact]: LabelForm refact , LabelForm Submit add

--- src/components/common/LabelForm/LabelForm.tsx
+++ src/components/common/LabelForm/LabelForm.tsx
@@ -1,14 +1,49 @@
-import React, { Children, ReactElement, ReactNode } from 'react';
+import React, {
+  Children,
+  createContext,
+  ReactElement,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import styles from './LabelForm.module.scss';
 import CheckIcon from '@/icons/icon/CheckIcon';
 import ContentField from '../ContentField/ContentField';
+import Button from '../Button/Button';
+
+interface LabelFormContextProps {
+  inputValue: Record<string, unknown>;
+  updateInputValue: (name: string, value: unknown) => void;
+}
+
+const LabelFormContext = createContext<LabelFormContextProps>({
+  inputValue: {},
+  updateInputValue: () => {},
+});
 
 interface LabelFormProps {
   children: ReactNode;
   className?: string;
 }
 const LabelForm = ({ children, className }: LabelFormProps) => {
-  return <div className={className}>{children}</div>;
+  const [inputValue, setInputValue] = useState<Record<string, unknown>>({});
+
+  const updateInputValue = useCallback((name: string, value: unknown) => {
+    setInputValue((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const providerValue = {
+    inputValue,
+    updateInputValue,
+  };
+
+  return (
+    <LabelFormContext.Provider value={providerValue}>
+      <div className={className}>{children}</div>
+    </LabelFormContext.Provider>
+  );
 };
 
 interface TitleProps {
@@ -66,28 +101,51 @@ const Label = ({ require, children, className }: LabelProps) => {
   );
 };
 
-interface ContentProps {
-  children: ReactElement<HTMLInputElement>;
+interface ContentProps<T> {
+  children: ReactNode;
   backgroundColor?: 'Gray' | 'White';
   border?: 'Border' | 'None';
   className?: string;
-  onClick?: () => void;
+  name: string;
+  data?: T;
 }
-const Content = ({
+const Content = <T,>({
   children,
   backgroundColor = 'White',
   border = 'Border',
-  onClick,
   className,
-}: ContentProps) => {
+  name,
+  data,
+}: ContentProps<T>) => {
+  const { updateInputValue } = useContext(LabelFormContext);
+
+  useEffect(() => {
+    if (data) {
+      updateInputValue(name, data);
+    }
+  }, [data, name]);
+
+  const addPropsToChildren = (children: React.ReactNode): React.ReactNode => {
+    return React.Children.map(children, (child) => {
+      if (React.isValidElement(child) && child.type === 'input') {
+        return React.cloneElement(child as ReactElement, {
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target;
+            updateInputValue(name, value);
+          },
+        });
+      }
+      return child;
+    });
+  };
+
   return (
     <ContentField
-      onClick={onClick}
-      className={className}
+      className={`${styles.Content} ${className}`}
       backgroundColor={backgroundColor}
       border={border}
     >
-      {children}
+      {addPropsToChildren(children)}
     </ContentField>
   );
 };
@@ -99,6 +157,24 @@ const Description = ({ children }: DescriptionProps) => {
   return <div className={styles.Description}>{children}</div>;
 };
 
+interface SubmitProps {
+  children: ReactNode;
+  disabled?: boolean;
+  callBack?: () => void;
+  className?: string;
+}
+const Submit = ({ children, disabled, callBack, className }: SubmitProps) => {
+  return (
+    <Button
+      buttonType={disabled ? 'Disabled' : 'Primary'}
+      className={className}
+      onClick={callBack}
+    >
+      {children}
+    </Button>
+  );
+};
+
 const SubLabelType = (<SubLabel />).type;
 const LabelType = (<Label />).type;
 
@@ -107,5 +183,6 @@ LabelForm.Label = Label;
 LabelForm.SubLabel = SubLabel;
 LabelForm.Content = Content;
 LabelForm.Description = Description;
+LabelForm.Submit = Submit;
 
 export default LabelForm;
