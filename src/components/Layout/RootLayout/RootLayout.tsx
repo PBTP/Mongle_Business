4e31c67a7c1536e883d13d30c0c4a50ff5b8bcb7
gbwlxhd97 <gbwlxhd97@naver.com>
2024-08-08 23:45:27 +0900

    fix: remove webview token logic

--- src/components/Layout/RootLayout/RootLayout.tsx
+++ src/components/Layout/RootLayout/RootLayout.tsx
@@ -10,18 +10,9 @@ type ContextType = {
 
 const RootLayout = () => {
   const token = useTokenStore((state) => state.accessToken);
-  const handleToken = () => {
-    logout();
-    setAccessToken('');
-  };
 
   return (
     <div className={styles.RootLayout}>
-      <div>token: {token}</div>
-      <div>
-        <button onClick={handleToken}>토큰값 삭제</button>
-        <button onClick={() => setAccessToken('TOKEN')}>토큰값 입력</button>
-      </div>
       <Outlet context={{ token } satisfies ContextType} />
     </div>
   );
--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import BottomNavigation from '@/components/Layout/BottomNavigation/BottomNavigation';
 import { useToken } from '@/components/Layout/RootLayout/RootLayout';
 import { useTokenStore } from '@/stores/useTokenStore';
 import { Navigate, Outlet, useOutletContext } from 'react-router';
@@ -5,7 +6,20 @@ import { Navigate, Outlet, useOutletContext } from 'react-router';
 const ProtectedRoute = () => {
   const { token } = useToken();
 
-  return <>{token ? <Outlet /> : <Navigate to="/" replace={true} />}</>;
+  return (
+    <>
+      {token ? (
+        <Outlet />
+      ) : (
+        <>
+          {/* TODO:로그인 로직이 들어오면 변경한다.
+          <Navigate to="/" replace={true} />
+        */}
+          <Outlet /> <BottomNavigation />
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProtectedRoute;
